881e2ea57668d6b73900dcf0194a5d636097bed5
adelgado24 <my>
2022-09-19 11:50:53 -0300

    pre-parcial

--- 04-transformaciones/tests.py
+++ 04-transformaciones/tests.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data_ejs/tarjetas.xlsx - Sheet1.csv')
 
 
 
 
 
 
-print("   hol a  ".strip())
+
--- 05-viz/tests.py
+++ 05-viz/tests.py
@@ -2,6 +2,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('data_nps.csv')
-
-df['detected_null'] = df['TAGS'].where(df['TAGS'].notnull(), 'No tags')
+df = pd.read_csv
